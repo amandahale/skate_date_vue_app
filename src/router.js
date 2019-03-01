753d2738a9106ab81e01b9bfab64029ef7eaa343
amanda <forshagonoff@gmail.com>
2019-03-01 12:16:54 -0600

    adds a LocationsNew page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LocationsIndex from './views/LocationsIndex.vue'
+import LocationsNew from './views/LocationsNew.vue'
 
 Vue.use(Router)
 
@@ -13,13 +14,18 @@ export default new Router({
       name: 'locations-index',
       component: LocationsIndex
     },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/locations/new',
+      name: 'locations-new',
+      component: LocationsNew
+    },
   ]
 })
---
+++ src/views/LocationsNew.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="locations-new">
+    <h2>Add A Location</h2>
+    <form v-on:submit.prevent="submit()">
+      <div>
+        Name: <input type="text" class="form-control" v-model="newLocationName">
+      </div>
+      <div>
+        Address: <input type="text" class="form-control" v-model="newLocationAddress">
+      </div>
+      <div>
+        Weblink: <input type="text" class="form-control" v-model="newLocationWeblink">
+      </div>      
+      <div>
+        Image Url: <input type="text" class="form-control" v-model="newLocationImageUrl">
+      </div>
+
+      <div>
+        <input type="submit" value="Create">
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+var axios = require("axios");
+
+export default {
+  data: function() {
+    return {
+      newLocationName: "",
+      newLocationAddress: "",
+      newLocationWeblink: "",
+      newLocationImageUrl: "",
+      errors: [],
+      status: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/locations").then(response => {
+      this.locations = response.data;
+    });
+  },
+  methods: {
+    submit: function() {
+      var locationParams = {
+        name: this.newLocationName,
+        address: this.newLocationAddress,
+        weblink: this.newLocationWeblink,
+        image_url: this.newLocationImageUrl
+      };
+      axios
+        .post("api/locations", locationParams)
+        .then(response => {
+          console.log("Location successfully created", response.data);
+          this.$router.push("/");
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+          this.status = error.response.status;
+        });
+    }
+  }
+};
+</script>
